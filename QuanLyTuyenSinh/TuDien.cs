a561094f9bacbc8ab0a17480b32f6cf2c31f27d7
Phan Hoàng Long <38026000+phlong97@users.noreply.github.com>
2023-03-31 09:30:22 +0700

    add tudien and _litedb

---
+++ QuanLyTuyenSinh/TuDien.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyTuyenSinh
+{
+    internal static class TuDien
+    {
+        public static string LITEDB_LOCAL_PATH = Path.Combine(Environment.CurrentDirectory, "QLTS.db");
+    }
+}
---
+++ QuanLyTuyenSinh/_LiteDb.cs
@@ -0,0 +1,33 @@
+﻿using LiteDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyTuyenSinh
+{
+    public class _LiteDb
+    {
+        private static LiteDatabase Db;
+        private _LiteDb() { }
+        public static LiteDatabase GetDb()
+        {
+            if (Db == null)
+                Db = new LiteDatabase(TuDien.LITEDB_LOCAL_PATH);
+            return Db;
+        }
+
+        public static bool UpsertObj<T>(T obj) where T : BaseClass
+        {
+            var coll = GetDb().GetCollection<T>();
+            return coll.Upsert(obj);
+        }
+        public static bool DeleteObj<T>(T obj) where T : BaseClass
+        {
+            var coll = GetDb().GetCollection<T>();
+
+            return coll.Delete(obj.Id);
+        }
+    }
+}
